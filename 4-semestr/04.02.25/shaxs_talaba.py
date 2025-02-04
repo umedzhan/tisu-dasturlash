2ae5c2633bbd70c7055ad7509530fe068479a3fa
umedzhan <163854602+umedzhan@users.noreply.github.com>
2025-02-04 17:15:24 +0500

    Update shaxs_talaba.py

--- 4-semestr/04.02.25/shaxs_talaba.py
+++ 4-semestr/04.02.25/shaxs_talaba.py
@@ -1,19 +1,29 @@
 class Shaxs:
+	odamlar_soni = 0
 	def __init__(self, ism, familiya):
 		self.ism = ism
 		self.familiya = familiya
+		Shaxs.odamlar_soni += 1
 
 class Talaba(Shaxs):
+	__talabalar_soni = 0
 	def __init__(self, ism, familiya, baho = 2):
 		super().__init__(ism, familiya)
 		self.__baho = baho
+		Talaba.__talabalar_soni += 1
 
 	def bahosi(self):
 		return self.__baho
 	def yangi(self, baho):
 		self.__baho = baho
 
-talaba1 = Talaba("Anvar", "Ashurov")
+	@classmethod
+	def soni(cls):
+		return cls.__talabalar_soni
+
+talaba1 = Talaba("Anvar", "Ashurov", 3)
 print(talaba1.bahosi())
 talaba1.yangi(5)
 print(talaba1.bahosi())
+print(talaba1.odamlar_soni)
+talaba2 = Talaba("Anvar", "Ashurov", 3)
