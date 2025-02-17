150ff6ef6b5126cb3da0075841c45190a0665f84
umedzhan <163854602+umedzhan@users.noreply.github.com>
2025-02-17 17:12:04 +0500

    Update 1-masala.py

--- 4-semestr/17.02.25/1-masala.py
+++ 4-semestr/17.02.25/1-masala.py
@@ -1,6 +1,11 @@
 import turtle as t
 
-t.speed(0)
+def stars(n):
+	for i in range(5):
+		t.forward(n)
+		t.right(144)
+
+t.speed(2)
 
 t.fillcolor('red')
 t.begin_fill()
@@ -62,6 +67,8 @@ t.forward(64)
 t.fillcolor('blue')
 t.begin_fill()
 
+
+
 t.forward(50)
 t.left(90)
 
@@ -75,5 +82,129 @@ t.forward(50)
 
 t.end_fill()
 
-t.hideturtle()
+t.up()
+t.goto(25, 32)
+t.down()
+
+t.fillcolor('white')
+t.begin_fill()
+
+t.circle(20)
+
+t.up()
+t.goto(35, 32)
+t.down()
+
+t.circle(20)
+
+t.up()
+t.goto(50, 32)
+t.down()
+t.end_fill()
+
+t.fillcolor('blue')
+t.begin_fill()
+
+t.color('blue')
+
+t.forward(25)
+t.left(90)
+
+t.forward(30)
+t.left(90)
+
+t.forward(50)
+t.left(90)
+
+t.forward(30)
+t.left(90)
+
+t.forward(40)
+
+t.end_fill()
+
+#yulduzlari
+
+t.up()
+t.goto(60, 50)
+
+t.fillcolor('white')
+t.begin_fill()
+stars(10)
+
+t.up()
+t.goto(70, 50)
+# t.down()
+
+stars(10)
+
+t.up()
+t.goto(80, 50)
+# t.down()
+
+stars(10)
+
+t.end_fill()
+
+
+t.up()
+t.goto(60, 40)
+
+t.fillcolor('white')
+t.begin_fill()
+stars(10)
+
+t.up()
+t.goto(70, 40)
+
+stars(10)
+
+t.up()
+t.goto(80, 40)
+
+stars(10)
+
+t.up()
+t.goto(90, 40)
+
+stars(10)
+
+t.end_fill()
+
+# t.down()
+
+stars(10)
+
+t.up()
+t.goto(60, 30)
+
+t.fillcolor('white')
+t.begin_fill()
+stars(10)
+
+t.up()
+t.goto(70, 30)
+
+stars(10)
+
+t.up()
+t.goto(80, 30)
+
+stars(10)
+
+t.up()
+t.goto(90, 30)
+
+stars(10)
+
+t.up()
+t.goto(100, 30)
+
+stars(10)
+
+t.end_fill()
+
+
+
+# t.hideturtle()
 t.done()
